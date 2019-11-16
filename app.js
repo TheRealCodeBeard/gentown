d42244c78d94efc3b71d30f022f76cdc454c6ceb
Phil <phil.d.harvey@gmail.com>
2019-11-16 12:12:36 +0000

    map generation moved to module

--- app.js
+++ app.js
@@ -3,47 +3,18 @@
     Each piece will go out to a Function to be triggered in Flow
 */
 
-const seedrandom = require('seedrandom');
 const name_generatore = require("./modules/genname.js");
+const generate_map = require("./modules/map_generator.js");
 const array_to_image = require("./modules/array_to_image.js");
 const fs = require('fs');
 
-seedrandom(Date.now().toLocaleString(), { global: true });
+console.log("Generating a names...");
+console.log(name_generatore());
 
-console.log("Generating some names...");
-for(let i=0;i<2;i++){
-    console.log(name_generatore());
-}
+console.log("Generating a map...");
+let {name,map,colours} = generate_map(name_generatore());
 
-const image_data = [
-    [3,3,3,3,3,3,3],
-    [0,0,0,0,0,0,0],
-    [0,1,1,1,0,0,0],
-    [0,1,0,1,1,0,2],
-    [0,1,1,1,0,0,0],
-    [0,0,0,0,0,0,0],
-    [3,3,3,3,3,3,3]
-];
-
-//could change this for a sprite map.
-const colour_map = [
-    "rgb(0,0,0)",// 0 is Default colour
-    "rgb(255,255,255)",
-    "rgb(255,0,0)",
-    "rgb(0,255,0)"
-];
-
-let size = 100;
-let random_image = new Array(size);
-for(var y=0;y<size;y++){
-    if(!random_image[y])random_image[y]=new Array(size);//init if we haven't already
-    for(var x=0;x<size;x++){
-        var colour_index = Math.round(Math.random()*(colour_map.length-1));
-        random_image[y][x] = colour_index;
-    }
-}
-
-const out = fs.createWriteStream("./images/test.png")
-
-array_to_image({w:300,h:300},colour_map,random_image,out);
-out.on('finish', () =>  console.log('The PNG file was created.'));
+console.log("Creating the image...");
+const out = fs.createWriteStream("./images/test.png");//Will need to be converted to blob
+array_to_image({w:300,h:300},colours,map,out);
+out.on('finish', () =>  console.log('Map image created'));
---
+++ modules/map_generator.js
@@ -0,0 +1,44 @@
+const seedrandom = require('seedrandom');
+
+const image_data = [
+    [3,3,3,3,3,3,3],
+    [0,0,0,0,0,0,0],
+    [0,1,1,1,0,0,0],
+    [0,1,0,1,1,0,2],
+    [0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0],
+    [3,3,3,3,3,3,3]
+];
+
+//could change this for a sprite map.
+const colour_map = [
+    "rgb(0,0,0)",// 0 is Default colour
+    "rgb(255,255,255)",
+    "rgb(255,0,0)",
+    "rgb(0,255,0)"
+];
+
+let random_map = function(size,rng){
+    let random_image = new Array(size);
+    for(var y=0;y<size;y++){
+        if(!random_image[y])random_image[y]=new Array(size);//init if we haven't already
+        for(var x=0;x<size;x++){
+            var colour_index = Math.round(rng()*(colour_map.length-1));
+            random_image[y][x] = colour_index;
+        }
+    }
+    return random_image;
+};
+
+
+let generate_map = function(name_seed){
+    console.log("Name seed:",name_seed);
+    let size = 150;
+    return {
+        name:name_seed,
+        map:random_map(size,seedrandom(name_seed)),
+        colours:colour_map
+    };
+};
+
+module.exports = generate_map
