8151539cb8c494b73131d6ad216b93f609651134
Phil <phil.d.harvey@gmail.com>
2019-11-21 05:31:51 +0000

    Having to write letters by vector

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ console.log(mod,"Generating a names...");
 console.log(mod,generate_name());
 
 console.log(mod,"Generating a map...");
-let {name,map,colours} = generate_map(generate_name());
+let generated_name = generate_name();
+generated_name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let {name,map,colours} = generate_map(generated_name);
 
 console.log(mod,"Creating the image...");
 const out = fs.createWriteStream("./images/map.png");//Will need to be converted to blob
+
 array_to_image({w:400,h:400},colours,name,map,out);
 out.on('finish', () =>  console.log(mod,'Map image created!\n'));
--- modules/array_to_image.js
+++ modules/array_to_image.js
@@ -6,6 +6,109 @@ let get_colour = function(map,index){
     else return map[0];
 };
 
+let boarder = function(ctx,size){
+    ctx.fillStyle = "rgba(50,50,50,0.5)";
+    ctx.fillRect(0,0,size.w,5);
+    ctx.fillRect(0,5,5,size.h);
+    ctx.fillRect(5,size.h-5,size.w,size.h);
+    ctx.fillRect(size.w-5,5,size.w,size.h-10);
+};
+
+let write_name = function(name,ctx,size){
+    name = name.toUpperCase();
+    ctx.fillStyle = "rgba(255,255,255,0.5)";
+    ctx.fillRect(5,5,size.w-10,25);
+    ctx.fillStyle = "rgb(0,0,0)";
+    let x = 10;
+    let y = 10;
+    let w = 5;
+    let h = 15;
+    name.split("").forEach(l=>{
+        switch (l){
+            case 'A':
+                ctx.beginPath();
+                ctx.moveTo(x,y+h);
+                ctx.lineTo(x+w/2,y);
+                ctx.lineTo(x+w,y+h);
+                ctx.stroke();
+                break;
+            case 'B':
+                ctx.beginPath();
+                ctx.moveTo(x,y);
+                ctx.lineTo(x,y+h);
+                ctx.lineTo(x+w,y+h);
+                ctx.lineTo(x,y+h/2);
+                ctx.lineTo(x+w,y);
+                ctx.lineTo(x,y);
+                ctx.stroke();
+                break;
+            case 'C':
+                ctx.beginPath();
+                ctx.moveTo(x+w,y);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h);
+                ctx.lineTo(x+w,y+h);
+                ctx.stroke();
+                break;
+            case 'D':
+                ctx.beginPath();
+                ctx.moveTo(x+(w-w*0.1),y+2);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h);
+                ctx.lineTo(x+(w-w*0.1),y+h-2);
+                ctx.lineTo(x+(w-w*-.1),y+2);
+                ctx.stroke();
+                break;
+            case 'E':
+                ctx.beginPath();
+                ctx.moveTo(x+w,y);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h/2);
+                ctx.lineTo(x+w/2,y+h/2);
+                ctx.moveTo(x,y+h/2);
+                ctx.lineTo(x,y+h);
+                ctx.lineTo(x+w,y+h);
+                ctx.stroke();
+                break;
+            case 'F':
+                ctx.beginPath();
+                ctx.moveTo(x+w,y);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h/2);
+                ctx.lineTo(x+w/2,y+h/2);
+                ctx.moveTo(x,y+h/2);
+                ctx.lineTo(x,y+h);
+                ctx.stroke();
+                break;
+            case 'G':
+                ctx.beginPath();
+                ctx.moveTo(x+w,y);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h);
+                ctx.lineTo(x+w,y+h);
+                ctx.lineTo(x+w,y+h/2);
+                ctx.stroke();
+                break;
+            case 'H':
+                ctx.beginPath();
+                ctx.moveTo(x,y);
+                ctx.lineTo(x,y);
+                ctx.lineTo(x,y+h);
+                ctx.moveTo(x+w,y);
+                ctx.lineTo(x+w,y+h);
+                ctx.moveTo(x,y+h/2);
+                ctx.lineTo(x+w,y+h/2);
+                ctx.stroke();
+                break;
+            default:
+                ctx.fillRect(x,y,w,h);
+        };
+        x = x+w+3;
+    });
+    //ctx.font = '20px courier';
+    //ctx.fillText(name,10, 22);
+};
+
 let generate_image = function(size,colour_map,name,data,out){
     const canvas = createCanvas(size.w, size.h);
     const ctx = canvas.getContext('2d');
@@ -25,12 +128,9 @@ let generate_image = function(size,colour_map,name,data,out){
             ctx.fillRect(x*block_w, y*block_h, block_w, block_h);
         }
     }
-    ctx.fillStyle = "rgba(255,255,255,0.5)";
-    ctx.fillRect(5,5,size.w-10,25);
-    ctx.fillStyle = "rgb(0,0,0)";
-    ctx.font = '20px courier';
-    ctx.fillText(name,10, 22);
 
+    boarder(ctx,size);
+    write_name(name,ctx,size);
     const stream = canvas.createPNGStream()
     stream.pipe(out);
 };
